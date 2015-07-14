bff507a04a9702c4875830745a0bd7cc73ff659b
Anders Wallenquist <anders.wallenquist@vertel.se>
2015-07-14 15:10:18 +0200

    Initial commit

---
+++ auth_admin/README.rst
@@ -0,0 +1,16 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+    :alt: License
+
+Unified password for admin
+==========================
+
+Using admin_password from /etc/odoo/openerp.conf as password for admin
+
+
+Credits
+=======
+
+Contributors
+------------
+
+ * Anders Wallenquist <anders.wallenquist@vertel.se>
---
+++ auth_admin/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#
+#
+##############################################################################
+
+
+{
+    'name': 'Auth Admin',
+    'version': '1.0',
+    'category': 'Tools',
+    'summary': 'Unified password for admin',
+    'author': 'Vertel AB',
+    'website': 'http://www.vertel.se',
+    'depends': ['base'],
+    'data': [], # 'res_users_view.xml',],
+    'installable': True,
+    'application': False,
+    #'auto_install': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
