8e4b4cefc36c455d5c94bdf03e41c7cbe5f21219
Miracle Ayodele <miracleaayodele@gmail.com>
2021-07-28 22:02:26 +0100

    user mail fix

--- user_mail_client/imp_res_users.py
+++ user_mail_client/imp_res_users.py
@@ -142,7 +142,7 @@ class res_users(models.Model):
 
         remote_user_id = self.remote_user(self)
         if remote_user_id:
-            postfix_alias_id = self.env['postfix.alias'].search([('user_id', '=', remote_user_id.id)])
+            postfix_alias_id = self.env['postfix.alias'].search([('user_id', '=', remote_user_id.id)], limit=1)
             postfix_alias_id.unlink()
             remote_user_id.write(record)
         else:
