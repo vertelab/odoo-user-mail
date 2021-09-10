87de92b28f883e77014a6e21d502ea7e1267899a
Fredrik Arvas, Vertel AB <61729317+fredrikarvas-vertel@users.noreply.github.com>
2021-09-10 06:09:54 +0200

    Update __manifest__.py

--- website_publisher_info/__manifest__.py
+++ website_publisher_info/__manifest__.py
@@ -8,14 +8,17 @@
 
 {
     'name': 'Website Publisher Info',
-    'version': '1.1',
+    'version': '14.0.1.0.0',
     'category': 'other',
     'summary': 'Information about the website for a website publisher',
     'author': 'Vertel AB',
     'website': 'http://www.vertel.se',
     'depends': ['base', 'user_password_tmp'],
     'data': ['website_publisher_info.xml',],
-
+    'description': ''' 
+    This module adds a webpage (/website/publisher_info) with information about the responsible person is for this website. 
+    This moduele is maintained from: https://github.com/vertelab/odoo-user-mail/tree/14.0/website_publisher_info 
+    ''',
     'installable': True,
     'application': False,
     #'auto_install': False,
