8798645640ecd9cfc0b245531e3ba27ba32b319f
iammiracle <miracleaayodele@gmail.com>
2022-04-05 15:40:33 +0100

    user mail server

--- user_mail_common/__manifest__.py
+++ user_mail_common/__manifest__.py
@@ -5,10 +5,9 @@
     'category': 'other',
     'summary': 'Administation of mail for users',
     'author': 'Vertel AB',
-    'website': 'http://www.vertel.se',
-    'depends': ['base', 'base_setup',],
+    'website': 'https://www.vertel.se',
+    'depends': ['base', 'base_setup'],
     'data': ['security/user_mail_security.xml', 'security/ir.model.access.csv', 'res_users_view.xml'],
     'installable': True,
     'application': False,
 }
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
