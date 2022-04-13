e321b17f52275057f62cba33eab790d6b54f0e02
iammiracle <miracleaayodele@gmail.com>
2022-04-13 11:20:26 +0100

    migrated user-mail-server to v12

---
+++ user_mail_server/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'User Mail Server',
+    'version': '12.0.0.1',
+    'category': 'other',
+    'summary': 'Administration of mail for users, serverside component',
+    'author': 'Vertel AB',
+    'website': 'https://www.vertel.se',
+    'depends': ['user_mail_common'],
+    'data': ['res_users_view.xml'],
+    'installable': True,
+    'application': True,
+}
