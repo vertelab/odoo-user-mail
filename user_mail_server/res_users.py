9149056757b6ca02507e61cbe7963511491b906c
Miracle Ayodele <miracleaayodele@gmail.com>
2022-04-14 06:25:53 +0100

    fixed user_mail_server v10 (#11)
    * fixed user_mail_server v10
    
    * added multi

--- user_mail_server/res_users.py
+++ user_mail_server/res_users.py
@@ -3,7 +3,7 @@
 #
 #    Copyright (C) 2015- Vertel AB (<http://www.vertel.se>).
 #
-#    This progrupdateam is free software: you can redistribute it and/or modify
+#    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
 #    published by the Free Software Foundation, either version 3 of the
 #    License, or (at your option) any later version.
@@ -17,35 +17,33 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-
-from openerp import models, fields, api, _
-from openerp.exceptions import Warning
 import logging
+
+from odoo import models, fields, api, _
+from odoo.exceptions import Warning
+from passlib.hash import sha512_crypt
+
 _logger = logging.getLogger(__name__)
 
-class res_users(models.Model):
+
+class ResUsers(models.Model):
     _inherit = 'res.users' 
 
     @api.model
     def create(self, values):
-		#_logger.info("In create serverside::::::::::::::")
-		ctx = self.env.context.copy()
-		ctx.update({'no_reset_password' : True})
-		return super(res_users, self.with_context(ctx)).create(values)
+        ctx = self.env.context.copy()
+        ctx.update({'no_reset_password': True})
+        return super(ResUsers, self.with_context(ctx)).create(values)
 
-    @api.one
-    def write(self,values):
-        #raise Warning(values)
-        if values.get('password') and not values.get('dovecot_password',False):
+    @api.multi
+    def write(self, values):
+        if values.get('password') and not values.get('dovecot_password', False):
             _logger.info('creates dovecot_password from %s' % values)
-            from passlib.hash import sha512_crypt
             values['dovecot_password'] = sha512_crypt.encrypt(values.get('password'))
-        return super(res_users, self).write(values)
+        return super(ResUsers, self).write(values)
 
-    #~ @api.one
-    #~ def unlink(self):
-        user_id = self.env['res.users'].search([('login','=',self.login)]).id
-        postfix_alias_id = self.env['postfix.alias'].search([('user_id', '=', self.id)]).unlink()
-        #~ self.postfix_alias_ids.unlink()
-        #~ return super(res_users, self).unlink()
+    @api.multi
+    def unlink(self):
+        self.env['postfix.alias'].search([('user_id', '=', self.id)]).unlink()
+        return super(ResUsers, self).unlink()
 
