ee61fbc56cbe22d4b2ab096e6586f0663067c76f
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2025-02-26 16:38:29 +0100

    Update __manifest__.py

--- user_mail_client/__manifest__.py
+++ user_mail_client/__manifest__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'User Mail: User Mail Client',
-    'version': '17.0.1.0.1',
+    'version': '1.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Administration of mail for users on client side.',
     # Categories can be used to filter modules in modules listing
